8211b894e5a22ad9a7286b7c2934168dc3814a6e
Trần Hồng Kỳ <0306231303@caothang.edu.vn>
2025-10-08 00:00:23 +0700

    BaiTapNhom02

---
+++ 23WebC_Nhom4_TW02/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _23WebC_Nhom4_TW02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
