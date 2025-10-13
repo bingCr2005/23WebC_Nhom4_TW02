98a5c737753cd189d6636c80fc14c379ac56b109
BaoNg11 <baonguyenqn1204@gmail.com>
2025-10-13 22:17:04 +0700

    Update full project - new version with product add feature

---
+++ 23WebC_Nhom4_TW02/Models/ICategoryDao.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace _23WebC_Nhom4_TW02.Models
+{
+    public interface ICategoryDao
+    {
+        List<Category> GetAllCategories();
+        Category GetCategoryById(int id);
+        bool AddCategory(Category category);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(int id);
+    }
+}
